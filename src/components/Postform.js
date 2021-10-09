88899f0b99a18477053ac1d34a710d361a10b031
cihat <cihat02020202@gmail.com>
2021-10-09 15:11:24 +0300

    Add components

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -1,38 +1,33 @@
-import React, { Component } from "react"
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createPost } from '../actions/postActions';
 
 class PostForm extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      title: "",
-      body: "",
-    }
+      title: '',
+      body: ''
+    };
 
-    this.onChange = this.onChange.bind(this)
-    this.onSubmit = this.onSubmit.bind(this)
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   onChange(e) {
-    this.setState({ [e.target.name]: e.target.value })
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   onSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
 
     const post = {
       title: this.state.title,
-      body: this.state.body,
-    }
+      body: this.state.body
+    };
 
-    fetch(`https://jsonplaceholder.typicode.com/posts`, {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(post),
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data))
+    this.props.createPost(post);
   }
 
   render() {
@@ -41,30 +36,35 @@ class PostForm extends Component {
         <h1>Add Post</h1>
         <form onSubmit={this.onSubmit}>
           <div>
-            <label>Title:</label>
+            <label>Title: </label>
             <br />
             <input
               type="text"
               name="title"
-              value={this.state.title}
               onChange={this.onChange}
+              value={this.state.title}
             />
           </div>
+          <br />
           <div>
-            <label>Body:</label>
+            <label>Body: </label>
             <br />
             <textarea
               name="body"
-              value={this.state.body}
               onChange={this.onChange}
-            ></textarea>
+              value={this.state.body}
+            />
           </div>
           <br />
           <button type="submit">Submit</button>
         </form>
       </div>
-    )
+    );
   }
 }
 
-export default PostForm
+PostForm.propTypes = {
+  createPost: PropTypes.func.isRequired
+};
+
+export default connect(null, { createPost })(PostForm);
