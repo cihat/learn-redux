f15ab22db6d1f2038e2a4d00f450d652015fb41c
cihat <cihat02020202@gmail.com>
2021-10-09 15:11:50 +0300

    Add redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import "./App.css"
-import Posts from "./components/Posts"
-import PostForm from "./components/Postform"
-import React, { Component } from "react"
-import { Provider } from "react-redux"
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Provider } from 'react-redux';
+import Posts from './components/Posts';
+import PostForm from './components/Postform';
+import store from './store';
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
           <PostForm />
           <hr />
           <Posts />
         </div>
       </Provider>
-    )
+    );
   }
 }
 
-export default App
+export default App;
